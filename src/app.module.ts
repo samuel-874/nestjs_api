d700e5e9b2750ef88016b657e5f249d49f34378a
Samuel <samuelab846@gmail.com>
2023-11-23 10:00:59 +0100

    changed databse type to posgtres

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,7 +17,7 @@ dotenv.config()
       UserModule,
       ProfileModule,
       TypeOrmModule.forRoot({
-        type: 'mysql',
+        type: 'postgres',
         host: process.env.DB_HOST,
         port: parseInt(process.env.DB_PORT),
         username: process.env.DB_USERNAME,
